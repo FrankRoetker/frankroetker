3fa85d7ce832b7815c35fbcfafb95ede67417afe
Frank Roetker <roetkefj@rose-hulman.edu>
2014-10-12 17:52:11 -0400

    need to start working on the images for the posts.

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ module.exports = {
   dev: {
     port: '8080',
     host: 'frankroetker.com'
+  },
+  posts: {
+    dir: "https://api.github.com/repos/frankroetker/frankroetker/contents/public/posts?ref=ReadingPosts",
+    raw: "https://raw.githubusercontent.com/FrankRoetker/frankroetker/ReadingPosts/"
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var fs = require('fs')
+  , cfg = require('../config.js')
   , Showdown = require('showdown')
-  , matter = require('gray-matter');
+  , matter = require('gray-matter')
+  , redis = require('redis')
+  , client = redis.createClient()
+  , rest = require('restler')
+  , showdown = new Showdown.converter();
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 module.exports = function(app){
-
 
   function readPost(post, callback){
     fs.readFile('./public/posts/'+post, 'utf8', callback);
@@ -15,51 +23,116 @@ module.exports = function(app){
     });
   });
 
-  app.param('post', /\w+/g);
+  app.get('/blog', function(req, res){
+    getAllFileData(function(posts){
+      getTop10Posts(posts, function(posts){
+        res.render('blog', {
+          title: 'Frank Roetker',
+          posts: posts
+        });
+      });
+    });
+  });
+
+  // app.param('post', /\w+/g);
 
   app.get('/blog/:post', function(req, res, next){
-    readPost("2014-09-10_Test-Post.md", function(err, data){
-      var converter = new Showdown.converter()
-        , file = matter(data);
-
-      if(!!err){
-        console.log(data);
-        res.redirect('/');
-        return;
-      } 
+    client.HVALS(req.params.post, function(err, results){
       res.render('story', {
-        title: 'Test',
+        title: results[0].title,
         posts: [{
-          title: file.data.title,
-          subtitle: file.data.subtitle,
-          author: file.data.author,
-          imageUrl: file.data.image,
-          body: converter.makeHtml(file.content)
+          data: JSON.parse(results[0]),
+          name: results[1],
+          content: showdown.makeHtml(results[2])
         }]
       });
     });
   });
 
-  app.get('/blog/test', function(req, res, next){
-    readPost("2014-09-10_Test-Post.md", function(err, data){
-      var converter = new Showdown.converter()
-        , file = matter(data);
+  function getPosts(callback){
+    client.SMEMBERS('posts', callback);
+  }
 
-      if(!!err){
-        console.log(data);
-        res.redirect('/');
-        return;
-      } 
-      res.render('story', {
-        title: 'Test',
-        posts: [{
-          title: file.data.title,
-          subtitle: file.data.subtitle,
-          author: file.data.author,
-          imageUrl: file.data.image,
-          body: converter.makeHtml(file.content)
-        }]
+  function getAllFileData(callback){
+    client.sort("posts", "by", "nosort", 
+      "get", "*->data", 
+      "get", "*->content", 
+      "get", "*->name",
+      function(err, results){
+        if(!!err){
+          console.log(err);
+          return;
+        }
+        var vals = [];
+        for(var i = 0; i < results.length; i+=3){
+          vals.push({
+            data: JSON.parse(results[i]),
+            content: showdown.makeHtml(results[i+1]),
+            name: results[i+2]
+          });
+        }
+        callback(vals);
       });
+  }
+
+  function restrictKey(posts, key, callback){
+    callback(posts.filter(function(e){
+      return e.data.keys.indexOf(key) >= 0;
+    }));
+  }
+
+  function getTop10Posts(posts, callback){
+    posts.sort(function(a, b){
+      if (a.data.date < b.data.date)
+        return 1;
+      if (a.data.date > b.data.date)
+        return -1;
+      return 0;
     });
-  });
+    callback(posts.slice(0, 10));
+  }
+
+  function searchPosts(restriction, callback){
+    getPosts(function(err, posts){
+      if(!!err){
+        console.log(err);
+        return;
+      }
+
+    });
+  }
+
+  function addOrUpdate(deets){
+    rest.get(cfg.posts.raw + deets.path).on('complete', function(result){
+      if(result instanceof Error){
+        console.log("Error", result.message);
+        this.retry(5000);
+      } else{
+        var data = matter(result);
+        client.SADD('posts', deets.name);
+        client.HMSET(deets.name, {
+          data: JSON.stringify(data.data),
+          content: data.content,
+          name: deets.name
+        });
+      }
+    });
+  }
+
+  function readPosts(){
+    // return;
+
+    rest.get(cfg.posts.dir).on('complete', function(result) {
+      if (result instanceof Error) {
+        console.log('Error:', result.message);
+        this.retry(5000); // try again after 5 sec
+      } else {
+        result.forEach(function(item){
+          addOrUpdate(item);
+        });
+      }
+    });
+  }; readPosts()
+
+  setInterval(readPosts, 86400000);
 }
